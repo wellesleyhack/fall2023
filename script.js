25b4ab55cef270e93026f50971c0841b587a71f9
anna-l-zhou <118085892+anna-l-zhou@users.noreply.github.com>
2023-09-05 00:08:17 -0400

    Update script.js
    Deployed new version javascript

--- script.js
+++ script.js
@@ -1,8 +1,332 @@
 // faq -----------------------------------------------------------------------------------------
-$(".question").each(function() {
-    $(this).click(function(){
-        $(this).find("p").toggle();
-        $(this).toggleClass("faq_exp");
-        //$(this).css('listStyleImage', $(this).hasClass("selected")?sel:unsel);    
-    })
+$(".faqcard").click(function () {
+  $(this).find(".answer").toggle();
+});
+
+// countdown ------------------------------------------------------------------------------------
+$(document).ready(function () {
+  function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    if (t < 0) {
+      return false;
+    }
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+  function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector(".days");
+    var hoursSpan = clock.querySelector(".hours");
+    var minutesSpan = clock.querySelector(".minutes");
+    var secondsSpan = clock.querySelector(".seconds");
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+      if (t) {
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+        minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+      } else {
+        clearInterval(timeinterval);
+      }
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
+  var deadline = new Date("November 4, 2023 00:00:00 GMT-4");
+  initializeClock("clockdiv", deadline);
+});
+
+// hamburger menu ------------------------------------------------------------------------------
+$(".nav-link-ham").click(function () {
+  if ($(".mobile-nav-links").css("display") == "none") {
+    $(".nav-link-ham svg").attr("fill", "white");
+    $(".nav-link-ham svg").removeClass("bi-list");
+    $(".nav-link-ham svg").addClass("bi-x-lg");
+    $(".nav-link-ham path").attr(
+      "d",
+      "M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"
+    );
+    $(".mobile-nav-links").css("display", "block");
+    $(".mobile-nav-links").css("visibility", "visible");
+    $("nav").css("margin-bottom", "10rem");
+    $("nav").css("background-color", "#304a8b");
+    $(".mlh-img").hide();
+  } else {
+    $(".nav-link-ham svg").attr("fill", "#304a8b");
+    $(".nav-link-ham svg").removeClass("bi-x-lg");
+    $(".nav-link-ham svg").addClass("bi-list");
+    $(".nav-link-ham path").attr(
+      "d",
+      "M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+    );
+    $(".mobile-nav-links").css("display", "none");
+    $(".mobile-nav-links").css("visibility", "hidden");
+    $("nav").css("margin-bottom", "0");
+    $("nav").css("background-color", "transparent");
+    $(".mlh-img").show();
+  }
+});
+
+$(".mobile-nav-links").click(function () {
+  $(".nav-link-ham svg").attr("fill", "#304a8b");
+  $(".nav-link-ham svg").removeClass("bi-x-lg");
+  $(".nav-link-ham svg").addClass("bi-list");
+  $(".nav-link-ham path").attr(
+    "d",
+    "M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+  );
+  $(".mobile-nav-links").css("display", "none");
+  $(".mobile-nav-links").css("visibility", "hidden");
+  $("nav").css("margin-bottom", "0");
+  $("nav").css("background-color", "transparent");
+  $(".mlh-img").show();
+});
+
+// meet the team -------------------------------------------------------------------------------------
+class teamMember {
+  constructor(team, name, year, position, photo) {
+    this.team = team;
+    this.name = name;
+    this.year = year;
+    this.position = position;
+    this.photo = photo;
+  }
+
+  addToDom() {
+    this.$div = $("<div>");
+    this.$div.addClass("member");
+
+    var imgdiv = $("<img>");
+    imgdiv.addClass("memberimg");
+    imgdiv.attr("src", this.photo);
+    imgdiv.attr("alt", "Photo of " + this.name);
+    this.$div.append(imgdiv);
+
+    var newdiv = $("<div>");
+    newdiv.addClass("hoverinfo");
+    newdiv.text(this.name + "\n" + this.year + "\n" + this.position);
+    this.$div.append(newdiv);
+
+    $(".teammembers").append(this.$div);
+  }
+}
+
+const phimg = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Silver_-_replace_this_image_male.svg/640px-Silver_-_replace_this_image_male.svg.png"
+
+const all = [
+  {
+    team: "director",
+    name: "Emily Zhang",
+    year: "",
+    position: "Co-Director",
+    photo: phimg,
+  },
+  {
+    team: "director",
+    name: "Kelly Cao",
+    year: "2024",
+    position: "Co-Director",
+    photo: "https://cdn.glitch.global/539b6955-9ff6-4ea6-9668-df589374b34e/KellyCao.JPG?v=1693227573020",
+  },
+  {
+    team: "director",
+    name: "Elizabeth Huang",
+    year: "2026",
+    position: "Vice President",
+    photo: "https://cdn.glitch.global/539b6955-9ff6-4ea6-9668-df589374b34e/ElizabethHuang.jpg?v=1693885984134",
+  },
+  {
+    team: "webdev",
+    name: "Anna Zhou",
+    year: "2025",
+    position: "Web Dev Lead",
+    photo: "https://cdn.glitch.global/539b6955-9ff6-4ea6-9668-df589374b34e/AnnaZhou.png?v=1693143794423",
+  },
+  {
+    team: "webdev",
+    name: "Ashi Kamra",
+    year: "2026",
+    position: "Web Dev",
+    photo: "https://cdn.glitch.global/539b6955-9ff6-4ea6-9668-df589374b34e/AshiKamra.png?v=1693228038277",
+  },
+  {
+    team: "dm",
+    name: "Sumin Heo",
+    year: "2025",
+    position: "Design/Marketing Lead",
+    photo: "https://cdn.glitch.global/539b6955-9ff6-4ea6-9668-df589374b34e/SuminHeo.JPG?v=1693227585032",
+  },
+  {
+    team: "dm",
+    name: "Jennifer Long",
+    year: "2025",
+    position: "Design/Marketing",
+    photo: "https://cdn.glitch.global/539b6955-9ff6-4ea6-9668-df589374b34e/JenniferLong.jpg?v=1693143799907",
+  },
+  {
+    team: "dm",
+    name: "Annie Chen",
+    year: "2026",
+    position: "Design/Marketing",
+    photo: "https://cdn.glitch.global/539b6955-9ff6-4ea6-9668-df589374b34e/AnnieChen.png?v=1693886045584",
+  },
+  {
+    team: "dm",
+    name: "Emma Lim",
+    year: "2024",
+    position: "Design/Marketing",
+    photo: "https://cdn.glitch.global/539b6955-9ff6-4ea6-9668-df589374b34e/EmmaLim.jpg?v=1693885988028",
+  },
+  {
+    team: "log",
+    name: "Kimberly Hsueh",
+    year: "2025",
+    position: "Logistics Lead",
+    photo: "https://cdn.glitch.global/539b6955-9ff6-4ea6-9668-df589374b34e/KimHsueh.jpeg?v=1693227577066",
+  },
+  {
+    team: "log",
+    name: "Tina Zhong",
+    year: "2026",
+    position: "Logistics",
+    photo: phimg,
+  },
+  {
+    team: "log",
+    name: "Mako Irisumi",
+    year: "2026",
+    position: "Logistics",
+    photo: "https://cdn.glitch.global/539b6955-9ff6-4ea6-9668-df589374b34e/MakoIrisumi.jpg?v=1693143803446",
+  },
+  {
+    team: "log",
+    name: "Sabrina Lu",
+    year: "2026",
+    position: "Logistics",
+    photo: "https://cdn.glitch.global/539b6955-9ff6-4ea6-9668-df589374b34e/SabrinaLu.JPG?v=1693143811370",
+  },
+  {
+    team: "log",
+    name: "Diya Khanna",
+    year: "2025",
+    position: "Logistics",
+    photo: phimg,
+  },
+  {
+    team: "sponsor",
+    name: "Mia Chao",
+    year: "2025",
+    position: "Sponsorships Lead",
+    photo: "https://cdn.glitch.global/539b6955-9ff6-4ea6-9668-df589374b34e/MiaChao.png?v=1693143808234",
+  },
+  {
+    team: "sponsor",
+    name: "Zora Levette",
+    year: "2024",
+    position: "Sponsorships",
+    photo: phimg,
+  },
+  {
+    team: "sponsor",
+    name: "Celine Yung",
+    year: "2026",
+    position: "Sponsorships",
+    photo: "https://cdn.glitch.global/539b6955-9ff6-4ea6-9668-df589374b34e/CelineYung.jpeg?v=1693885930316",
+  },
+  {
+    team: "sponsor",
+    name: "Dora Li",
+    year: "2026",
+    position: "Sponsorships",
+    photo: phimg,
+  },
+  {
+    team: "sponsor",
+    name: "Emilie Zhang",
+    year: "",
+    position: "Sponsorships",
+    photo: phimg,
+  },
+  {
+    team: "treasury",
+    name: "Caitlin-Rose Ibe",
+    year: "2025",
+    position: "Treasury Lead",
+    photo: "https://cdn.glitch.global/539b6955-9ff6-4ea6-9668-df589374b34e/CaitlinIbe.jpg?v=1693143796742",
+  },
+  {
+    team: "treasury",
+    name: "Jaci Cotaquispe",
+    year: "2025",
+    position: "Treasury",
+    photo: phimg,
+  },
+];
+
+function teamClickHandler(team) {
+  $(".teammembers").empty();
+  for (let i = 0; i < all.length; i++) {
+    var a = all[i];
+    if (a["team"] === team.data.param1) {
+      var newmember = new teamMember(
+        a["team"],
+        a["name"],
+        a["year"],
+        a["position"],
+        a["photo"]
+      );
+      newmember.addToDom();
+    }
+  }
+  $(".teamtitle").css("background-color", "white");
+  $(".teamtitle").css("color", "black");
+  $(this).css("background-color", "#287F9F");
+  $(this).css("color", "white");
+}
+
+function allClickHandler() {
+  $(".teammembers").empty();
+  for (let i = 0; i < all.length; i++) {
+    var a = all[i];
+      var newmember = new teamMember(
+        a["team"],
+        a["name"],
+        a["year"],
+        a["position"],
+        a["photo"]
+      );
+      newmember.addToDom();
+  }
+  $(".teamtitle").css("background-color", "white");
+  $(".teamtitle").css("color", "black");
+  $(".allmembers").css("background-color", "#287F9F");
+  $(".allmembers").css("color", "white");
+}
+
+$(document).ready(allClickHandler)
+$(".allmembers").click(allClickHandler);
+$(".webdevs").click({param1: "webdev"}, teamClickHandler);
+$(".directors").click({param1: "director"}, teamClickHandler);
+$(".dms").click({param1: "dm"}, teamClickHandler);
+$(".logistics").click({param1: "log"}, teamClickHandler);
+$(".sponsorships").click({param1: "sponsor"}, teamClickHandler);
+$(".treasuries").click({param1: "treasury"}, teamClickHandler);
+
+$(document).on("mouseenter", ".member", function () {
+  $(this).find(".hoverinfo").css("display", "flex");
+});
+
+$(document).on("mouseleave", ".member", function () {
+  $(this).find(".hoverinfo").hide();
 });
